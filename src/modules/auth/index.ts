eac8c78d034f6cd1e85b0206d25898cee4fa9f93
Damir <damiratamuratov07@gmail.com>
2024-07-19 18:50:15 +0500

    initial commit

--- src/modules/auth/index.ts
+++
@@ -1,2 +0,0 @@
-export { AuthPage } from './auth-page'
-export { authSlice } from './model/auth.slice'
--- src/modules/customers/index.ts
+++
@@ -1,6 +0,0 @@
-export { CustomerPage } from './customer-page'
-export { CustomersPage } from './customers-page'
-export {
-	likedCustomersSlice,
-	type LikedCustomers,
-} from './model/likedCustomers.slice'
